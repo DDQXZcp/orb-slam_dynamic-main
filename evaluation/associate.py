6b11c4c3763c73777c2f195dfdaf5d373bb6fb71
DDQXZcp <347877478@qq.com>
2024-03-11 17:06:31 +1100

    Do some improvements in evaluate_gui Implement evaluate_rpe Refactor code Implement rotation in visualization Experiment and find out the rotation won't affect ATE and RPE

--- evaluation/associate.py
+++ evaluation/associate.py
@@ -1,93 +1,46 @@
-#!/usr/bin/python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2013, Juergen Sturm, TUM
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of TUM nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Requirements: 
-# sudo apt-get install python-argparse
-
-"""
-The Kinect provides the color and depth images in an un-synchronized way. This means that the set of time stamps from the color images do not intersect with those of the depth images. Therefore, we need some way of associating color images to depth images.
-
-For this purpose, you can use the ''associate.py'' script. It reads the time stamps from the rgb.txt file and the depth.txt file, and joins them by finding the best matches.
-"""
-
-import argparse
-import sys
-import os
-import numpy
-
+import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 def read_file_list(filename):
-    """
-    Reads a trajectory from a text file. 
-    
-    File format:
-    The file format is "stamp d1 d2 d3 ...", where stamp denotes the time stamp (to be matched)
-    and "d1 d2 d3.." is arbitary data (e.g., a 3D position and 3D orientation) associated to this timestamp. 
-    
-    Input:
-    filename -- File name
-    
-    Output:
-    dict -- dictionary of (stamp,data) tuples
-    
-    """
-    file = open(filename)
-    data = file.read()
-    lines = data.replace(","," ").replace("\t"," ").split("\n") 
-    list = [[v.strip() for v in line.split(" ") if v.strip()!=""] for line in lines if len(line)>0 and line[0]!="#"]
-    list = [(float(l[0]),l[1:]) for l in list if len(l)>1]
+    with open(filename) as file:
+        data = file.read()
+    lines = data.replace(",", " ").replace("\t", " ").split("\n")
+    list = [[v.strip() for v in line.split(" ") if v.strip() != ""] for line in lines if len(line) > 0 and line[0] != "#"]
+    list = [(float(l[0]), l[1:]) for l in list if len(l) > 1]
     return dict(list)
 
-def associate(first_list, second_list,offset,max_difference):
-    """
-    Associate two dictionaries of (stamp,data). As the time stamps never match exactly, we aim 
-    to find the closest match for every input tuple.
+def read_file_list_rotate(filename, x=0, y=0, z=0):
+    # Convert rotation angles from degrees to radians
+    rotation_radians = np.radians([x, y, z])
+    rotation = R.from_euler('xyz', rotation_radians)
     
-    Input:
-    first_list -- first dictionary of (stamp,data) tuples
-    second_list -- second dictionary of (stamp,data) tuples
-    offset -- time offset between both dictionaries (e.g., to model the delay between the sensors)
-    max_difference -- search radius for candidate generation
+    with open(filename) as file:
+        data = file.read()
+        
+    lines = data.replace(",", " ").replace("\t", " ").split("\n")
+    data_list = [[v.strip() for v in line.split(" ") if v.strip() != ""] for line in lines if len(line) > 0 and line[0] != "#"]
+    rotated_data = {}
 
-    Output:
-    matches -- list of matched tuples ((stamp1,data1),(stamp2,data2))
-    
-    """
-    first_keys = first_list.keys()
-    second_keys = second_list.keys()
-    potential_matches = [(abs(a - (b + offset)), a, b) 
-                         for a in first_keys 
-                         for b in second_keys 
+    for line in data_list:
+        if len(line) >= 8:  # Ensure it's a full data line with timestamp and position/orientation data
+            stamp = float(line[0])
+            position = np.array(line[1:4], dtype=np.float64)
+            quaternion = line[4:8]  # Not modifying orientation in this example
+            
+            # Apply rotation to position
+            rotated_position = rotation.apply(position)
+            
+            # Update data dictionary with rotated position
+            rotated_data[stamp] = list(rotated_position) + quaternion
+
+    return rotated_data
+
+def associate(first_list, second_list, offset, max_difference):
+    first_keys = list(first_list.keys())
+    second_keys = list(second_list.keys())
+    potential_matches = [(abs(a - (b + offset)), a, b)
+                         for a in first_keys
+                         for b in second_keys
                          if abs(a - (b + offset)) < max_difference]
     potential_matches.sort()
     matches = []
@@ -96,45 +49,11 @@ def associate(first_list, second_list,offset,max_difference):
             first_keys.remove(a)
             second_keys.remove(b)
             matches.append((a, b))
-    
+
     matches.sort()
 
     print("first_list: ", len(first_list))
     print("second_list: ", len(second_list))
     print("matches: ", len(matches))
 
-    # output_file = "matches.txt"
-
-    # # Save matches to a text file
-    # with open(output_file, 'w') as f:
-    #     for match in matches:
-    #         f.write("%s %s\n" % (match[0], match[1]))
-
     return matches
-
-if __name__ == '__main__':
-    
-    # parse command line
-    parser = argparse.ArgumentParser(description='''
-    This script takes two data files with timestamps and associates them   
-    ''')
-    parser.add_argument('first_file', help='first text file (format: timestamp data)')
-    parser.add_argument('second_file', help='second text file (format: timestamp data)')
-    parser.add_argument('--first_only', help='only output associated lines from first file', action='store_true')
-    parser.add_argument('--offset', help='time offset added to the timestamps of the second file (default: 0.0)',default=0.0)
-    parser.add_argument('--max_difference', help='maximally allowed time difference for matching entries (default: 0.02)',default=0.02)
-    args = parser.parse_args()
-
-    first_list = read_file_list(args.first_file)
-    second_list = read_file_list(args.second_file)
-
-    matches = associate(first_list, second_list,float(args.offset),float(args.max_difference))    
-
-    if args.first_only:
-        for a,b in matches:
-            print("%f %s"%(a," ".join(first_list[a])))
-    else:
-        for a,b in matches:
-            print("%f %s %f %s"%(a," ".join(first_list[a]),b-float(args.offset)," ".join(second_list[b])))
-            
-        
--- evaluation/evaluate_gui.py
+++ evaluation/evaluate_gui.py
@@ -1,122 +1,25 @@
 import gradio as gr
-import numpy
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import plotly.graph_objects as go
+import associate
+import evaluate_ate
+import evaluate_rpe
 
 # Gradio 3.48
 # Python 3
 
-offset = 0.0
-scale = 1.0
-max_difference = 0.02
+path_text = """
+## Ground Truth Trajectory Paths
 
-def read_file_list(filename):
-    with open(filename) as file:
-        data = file.read()
-    lines = data.replace(",", " ").replace("\t", " ").split("\n")
-    list = [[v.strip() for v in line.split(" ") if v.strip() != ""] for line in lines if len(line) > 0 and line[0] != "#"]
-    list = [(float(l[0]), l[1:]) for l in list if len(l) > 1]
-    return dict(list)
+- Path 1: `/home/ubuntu/Downloads/rgbd_dataset_freiburg3_sitting_static/groundtruth.txt`
+- Path 2: `/home/ubuntu/Downloads/rgbd_dataset_freiburg3_walking_xyz/groundtruth.txt`
+- Path 3: `/home/ubuntu/Downloads/rgbd_dataset_freiburg3_long_office_household/groundtruth.txt`
 
-def associate(first_list, second_list, offset, max_difference):
-    first_keys = list(first_list.keys())
-    second_keys = list(second_list.keys())
-    potential_matches = [(abs(a - (b + offset)), a, b)
-                         for a in first_keys
-                         for b in second_keys
-                         if abs(a - (b + offset)) < max_difference]
-    potential_matches.sort()
-    matches = []
-    for diff, a, b in potential_matches:
-        if a in first_keys and b in second_keys:
-            first_keys.remove(a)
-            second_keys.remove(b)
-            matches.append((a, b))
+**Note:** These paths are for display purposes only and cannot be edited.
+"""
 
-    matches.sort()
-
-    print("first_list: ", len(first_list))
-    print("second_list: ", len(second_list))
-    print("matches: ", len(matches))
-
-    return matches
-
-def align(model, data):
-    """Align two trajectories using the method of Horn (closed-form).
-    
-    Input:
-    model -- first trajectory (3xn)
-    data -- second trajectory (3xn)
-    
-    Output:
-    rot -- rotation matrix (3x3)
-    trans -- translation vector (3x1)
-    trans_error -- translational error per point (1xn)
-    
-    """
-    numpy.set_printoptions(precision=3, suppress=True)
-    model_zerocentered = model - model.mean(1)
-    data_zerocentered = data - data.mean(1)
-    
-    W = numpy.zeros((3, 3))
-    for column in range(model.shape[1]):
-        W += numpy.outer(model_zerocentered[:, column], data_zerocentered[:, column])
-    U, d, Vh = numpy.linalg.svd(W.transpose())
-    S = numpy.matrix(numpy.identity(3))
-    if(numpy.linalg.det(U) * numpy.linalg.det(Vh) < 0):
-        S[2, 2] = -1
-    rot = U*S*Vh
-    trans = data.mean(1) - rot * model.mean(1)
-    
-    model_aligned = rot * model + trans
-    alignment_error = model_aligned - data
-    
-    trans_error = numpy.sqrt(numpy.sum(numpy.multiply(alignment_error, alignment_error), 0)).A[0]
-    
-    return rot, trans, trans_error
-
-def compute_ate(groundtruth_path, trajectory1_path, trajectory2_path):
-
-    first_list = read_file_list(groundtruth_path)
-    second_list = read_file_list(trajectory1_path)
-    third_list = read_file_list(trajectory2_path)
-    print(groundtruth_path, trajectory1_path, trajectory2_path)
-
-    second_keys = sorted(second_list.keys())
-    third_keys = sorted(third_list.keys())
-    common_start = max(second_keys[0], third_keys[0])
-    common_end = min(second_keys[-1], third_keys[-1])
-    print("common_start: ",common_start, "common_end: ", common_end)
-    second_list = {k: v for k, v in second_list.items() if common_start <= k <= common_end}
-    third_list = {k: v for k, v in third_list.items() if common_start <= k <= common_end}
-
-    matches_2 = associate(first_list, second_list, offset, max_difference)
-    if len(matches_2) < 2:
-        sys.exit("Couldn't find matching timestamp pairs between ground truth and second trajectory! Did you choose the correct sequence?")
-    
-    first_xyz_2 = numpy.matrix([[float(value) for value in first_list[a][0:3]] for a, b in matches_2]).transpose()
-    second_xyz = numpy.matrix([[float(value) * scale for value in second_list[b][0:3]] for a, b in matches_2]).transpose()
-    rot_2, trans_2, trans_error_2 = align(second_xyz, first_xyz_2)
-    ATE_2 = numpy.sqrt(numpy.dot(trans_error_2, trans_error_2) / len(trans_error_2))
-
-    # Associate, align, and calculate ATE for the third trajectory
-    matches_3 = associate(first_list, third_list, offset, max_difference)
-    if len(matches_3) < 2:
-        sys.exit("Couldn't find matching timestamp pairs between ground truth and third trajectory! Did you choose the correct sequence?")
-    
-    first_xyz_3 = numpy.matrix([[float(value) for value in first_list[a][0:3]] for a, b in matches_3]).transpose()
-    third_xyz = numpy.matrix([[float(value) * scale for value in third_list[b][0:3]] for a, b in matches_3]).transpose()
-    rot_3, trans_3, trans_error_3 = align(third_xyz, first_xyz_3)
-    ATE_3 = numpy.sqrt(numpy.dot(trans_error_3, trans_error_3) / len(trans_error_3))
-
-    print("ATE for the second trajectory: {:.3f} m".format(ATE_2))
-    print("ATE for the third trajectory: {:.3f} m".format(ATE_3))
-
-    return ATE_2, ATE_3
-
-def visualize_trajectory_plotly(path, name):
-    data_dict = read_file_list(path)
+def visualize_trajectory_plotly(data_dict, name):
     positions = [(float(data[0]), float(data[1]), float(data[2])) for _, data in data_dict.items()]
     x, y, z = zip(*positions)
     
@@ -138,16 +41,18 @@ def visualize_trajectory_plotly(path, name):
 
     fig.update_layout(margin=dict(l=0, r=0, b=0, t=0),
                       scene=dict(xaxis_title='X', yaxis_title='Y', zaxis_title='Z'))
+    
     return fig
 
 with gr.Blocks() as app:
     gr.Markdown("# Trajectory Analysis")
+    path_display = gr.Markdown(path_text)
     with gr.Row():
-        groundtruth_path = gr.Textbox(label="Groundtruth Trajectory Path", value = "/home/ubuntu/Downloads/rgbd_dataset_freiburg3_sitting_static/groundtruth.txt")
-        trajectory1_path = gr.Textbox(label="Trajectory 1 Path", value = "/home/ubuntu/orb-slam_dynamic-main/FullFrameTrajectoryTUM_sit_masked.txt")
-        trajectory2_path = gr.Textbox(label="Trajectory 2 Path", value = "/home/ubuntu/orb-slam_dynamic-main/FullFrameTrajectoryTUM_sit_unmasked.txt")
+        groundtruth_path = gr.Textbox(label="Groundtruth Trajectory Path", value = "/home/ubuntu/Downloads/rgbd_dataset_freiburg3_walking_xyz/groundtruth.txt")
+        trajectory1_path = gr.Textbox(label="Trajectory 1 Path", value = "/home/ubuntu/orb-slam_dynamic-main/FullFrameTrajectoryTUM_walk_masked.txt")
+        trajectory2_path = gr.Textbox(label="Trajectory 2 Path", value = "/home/ubuntu/orb-slam_dynamic-main/FullFrameTrajectoryTUM_walk_unmasked.txt")
     with gr.Row():
-        compute_ate_btn = gr.Button("Compute ATE")
+        compute_btn = gr.Button("Compute ATE & RPE")
         visualize_btn = gr.Button("Visualize Trajectories")
     with gr.Row():
         plot_output_gt = gr.Plot(label="Groundtruth Trajectory Visualization")
@@ -156,24 +61,68 @@ with gr.Blocks() as app:
     with gr.Row():
         ate_output1 = gr.Text(label="ATE for Trajectory 1")
         ate_output2 = gr.Text(label="ATE for Trajectory 2")
+        ate_pair1 = gr.Text(label="Match Pairs for Trajectory 1")
+        ate_pair2 = gr.Text(label="Match Pairs for Trajectory 2")
+    with gr.Row():
+        rpe_output1 = gr.Text(label="RPE for Trajectory 1")
+        rpe_output2 = gr.Text(label="RPE for Trajectory 2")
+        rpe_pair1 = gr.Text(label="Match Pairs for Trajectory 1")
+        rpe_pair2 = gr.Text(label="Match Pairs for Trajectory 2")
 
     def visualize_all(groundtruth_path, trajectory1_path, trajectory2_path):
-        fig_gt = visualize_trajectory_plotly(groundtruth_path, "Groundtruth")
-        fig_t1 = visualize_trajectory_plotly(trajectory1_path, "Trajectory 1")
-        fig_t2 = visualize_trajectory_plotly(trajectory2_path, "Trajectory 2")
+        # Load and parse the trajectory data
+        gt_data = associate.read_file_list_rotate(groundtruth_path, x=90)
+        t1_data = associate.read_file_list(trajectory1_path)
+        t2_data = associate.read_file_list(trajectory2_path)
+        
+        gt_timestamps = sorted(map(float, gt_data.keys()))
+        t1_timestamps = sorted(map(float, t1_data.keys()))
+        t2_timestamps = sorted(map(float, t2_data.keys()))
+        
+        # Find the maximum of the minimum timestamps and the minimum of the maximum timestamps
+        common_start = max(gt_timestamps[0], t1_timestamps[0], t2_timestamps[0])
+        common_end = min(gt_timestamps[-1], t1_timestamps[-1], t2_timestamps[-1])
+        
+        # Ensure there is overlap
+        if common_start >= common_end:
+            raise ValueError("No overlapping timestamps found among the trajectories.")
+        
+        # Filter data for common timeframe
+        gt_data_common = {k: v for k, v in gt_data.items() if common_start <= k <= common_end}
+        t1_data_common = {k: v for k, v in t1_data.items() if common_start <= k <= common_end}
+        t2_data_common = {k: v for k, v in t2_data.items() if common_start <= k <= common_end}
+        
+        # Visualize trimmed trajectories
+        fig_gt = visualize_trajectory_plotly(gt_data_common, "Groundtruth")
+        fig_t1 = visualize_trajectory_plotly(t1_data_common, "Trajectory 1")
+        fig_t2 = visualize_trajectory_plotly(t2_data_common, "Trajectory 2")
         return fig_gt, fig_t1, fig_t2
+
 
     visualize_btn.click(
         fn=visualize_all,
         inputs=[groundtruth_path, trajectory1_path, trajectory2_path],
         outputs=[plot_output_gt, plot_output_t1, plot_output_t2]
     )
+
+    def compute_ate_and_rpe(groundtruth_path, trajectory1_path, trajectory2_path):
+    # Assuming evaluate_ate.compute_ate returns two ATE results and two sets of pairs
+        ate_result1, ate_result2, ate_pairs_result1, ate_pairs_result2 = evaluate_ate.compute_ate(
+        groundtruth_path, trajectory1_path, trajectory2_path)
     
-    compute_ate_btn.click(
-        fn=compute_ate,
+    # Assuming evaluate_rpe.compute_rpe returns two RPE results and two sets of pairs
+        rpe_result1, rpe_result2, rpe_pairs_result1, rpe_pairs_result2 = evaluate_rpe.compute_rpe(
+        groundtruth_path, trajectory1_path, trajectory2_path)
+    
+    # Return all results in the order expected by the Gradio interface
+        return ate_result1, ate_result2, ate_pairs_result1, ate_pairs_result2, \
+           rpe_result1, rpe_result2, rpe_pairs_result1, rpe_pairs_result2
+    
+    compute_btn.click(
+        fn=compute_ate_and_rpe,
         inputs=[groundtruth_path, trajectory1_path, trajectory2_path],
-        outputs=[ate_output1, ate_output2]
-    )
+        outputs=[ate_output1, ate_output2, ate_pair1, ate_pair2, rpe_output1, rpe_output2, rpe_pair1, rpe_pair2]
+    ) 
 
 if __name__ == "__main__":
     app.launch()
